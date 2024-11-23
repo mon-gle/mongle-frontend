d64d3f82b392aa17cfd4b27eea1ee0e3c98d2b8d
westofsky <westofsky159@gmail.com>
2024-11-23 22:20:13 +0900

    feat: 라우팅 추가

--- src/components/detail/BookActions.tsx
+++ src/components/detail/BookActions.tsx
@@ -1,10 +1,18 @@
 import { IconBookBlack, IconBrainYellow } from '@/assets/icons';
 import { Text } from '@/components/common/Text';
+import { useNavigate } from 'react-router-dom';
 
 export const BookActions = () => {
+  const navigate = useNavigate();
+  const handleReadStory = () => {
+    navigate('/story/1');
+  };
   return (
     <div className="flex gap-16pxr z-10">
-      <div className="flex flex-col gap-10pxr">
+      <div
+        className="flex flex-col gap-10pxr cursor-pointer"
+        onClick={handleReadStory}
+      >
         <div className="w-72pxr h-72pxr flex items-center justify-center bg-white rounded-full">
           <IconBookBlack />
         </div>
